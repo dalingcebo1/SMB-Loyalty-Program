71e5398f11e084439004202b666f900abf9b9b89
Dalingcebo Ngubane <41449732+dalingcebo1@users.noreply.github.com>
2025-05-01 19:39:12 +0000

    Enabled Google Auth so far

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,7 +1,9 @@
 // src/App.tsx
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-
+import Login from './pages/Login';
+//import Signup from './pages/Signup';
+//import Claimed from './pages/Claimed';
 import OrderForm from "./pages/OrderForm";
 import PaystackPayment from "./pages/PaystackPayment";
 import OrderConfirmation from "./pages/OrderConfirmation";
@@ -9,6 +11,10 @@ import OrderConfirmation from "./pages/OrderConfirmation";
 
 const App: React.FC = () => (
   <Routes>
+    <Route path="/login" element={<Login />} />
+    {/* <Route path="/signup" element={<Signup />} /> */}
+    {/* <Route path="/claimed" element={<Claimed />} /> */}
+    {/* Add your other routes here */}    
     <Route path="/" element={<OrderForm />} />
     <Route path="/payment" element={<PaystackPayment />} />
     <Route path="/order/confirmation" element={<OrderConfirmation />} />
